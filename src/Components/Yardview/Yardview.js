cde8eefe78b29ab5b37c4dd3ece735006961ffce
rohitCode45 <rohitmudgal2000z@gmail.com>
2023-09-15 18:31:58 +0530

    feat: added new ateli dummy svg in app

--- src/Components/Yardview/Yardview.js
+++ src/Components/Yardview/Yardview.js
@@ -3,6 +3,7 @@ import './Yardview.scss'
 import Header from '../../Globel Components/Header/Header'
 import { useSelector } from 'react-redux'
 import { selectLiveData } from '../../Globel Utils/globeldataSlice'
+import { ReactComponent as ATELI } from './SVG/AteliSvg.svg'
 
 function Yardview() {
   const liveDataaaa = useSelector(selectLiveData)
@@ -12,6 +13,7 @@ function Yardview() {
       <Header label={'Yardview'} />
       <div className='dummy-svg-container'>
         <div className='dummy-svg'>Coming soon...</div>
+        {/* <ATELI /> */}
       </div>
     </div>
   )
